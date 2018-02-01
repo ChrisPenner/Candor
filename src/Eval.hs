29afa50ba2fa79dfed6822399e40e154aee080c0
Chris Penner <christopher.penner@gmail.com>
2018-01-31 20:54:01 -0600

    Change eval to use Either for errors

--- src/Eval.hs
+++ src/Eval.hs
@@ -7,55 +7,57 @@ import qualified Data.Map as M
 
 type Bindings = Map String AST
 
-globals :: Bindings
-globals = M.fromList
-            [ ("+", Builtin "+")
-            , ("-", Builtin "-")
-            , ("*", Builtin "*")
-            , ("=", Builtin "=")
-            , ("merge", Builtin "merge")
-            ]
+primitives :: Bindings
+primitives = M.fromList . fmap embed $ 
+  [ "+" , "-" , "*" , "=" , "merge" ]
+    where embed x = (x, Builtin x)
 
-eval :: AST -> AST
-eval ast = eval' globals ast
+eval :: AST -> Either String AST
+eval ast = eval' primitives ast
 
-eval' :: Bindings -> AST -> AST
+eval' :: Bindings -> AST -> Either String AST
 eval' bindings (Symbol name) =
   case M.lookup name bindings of
     Just expr -> eval' bindings expr
     Nothing -> error $ "no symbol in scope for: " ++ name
 
 eval' bindings (Appl h []) = eval' bindings h
-eval' bindings (Appl ((eval' bindings) -> Bindings binds) [expr]) = eval' (bindings <> binds) expr
-eval' bindings (Appl ((eval' bindings) -> Builtin name) args) =
-  builtin name (eval' bindings <$> args)
-eval' bindings (Appl ((eval' bindings) -> Func binders expr) args) =
-  let argSymbols = assertBinders <$> binders
-      newBindings = bindings <> M.fromList (zip argSymbols (eval' bindings <$> args))
-    in eval' newBindings expr
+eval' bindings (Appl ((eval' bindings) -> Right (Bindings binds)) [expr]) = eval' (bindings <> binds) expr
+eval' bindings (Appl ((eval' bindings) -> Right (Builtin name)) args) =
+  (traverse (eval' bindings) args) >>= builtin name 
+eval' bindings (Appl ((eval' bindings) -> Right (Func binders expr)) args) = do
+  newArgs <- traverse (eval' bindings) args
+  argSymbols <- traverse assertBinders binders
+  let newBindings =  bindings <> M.fromList (zip argSymbols newArgs)
+  eval' newBindings expr
 eval' _ (Appl expr _) =
-  error $ "expected function not expression: " ++ show expr
-eval' bindings (List elems) = List (eval' bindings <$> elems)
-eval' _ f@(Func _ _) = f
-eval' _ s@(Str _) = s
-eval' _ n@(Number _) = n
-eval' _ b@(Binder _) = b
-eval' _ b@(Builtin _) = b
-eval' _ b@(Bindings _) = b
+  Left $ "expected function not expression: " ++ show expr
+eval' bindings (List elems) = List <$> (traverse (eval' bindings) elems)
+eval' _ f@(Func _ _) = Right $ f
+eval' _ s@(Str _) = Right $ s
+eval' _ n@(Number _) = Right $ n
+eval' _ b@(Binder _) = Right $ b
+eval' _ b@(Builtin _) = Right $ b
+eval' _ b@(Bindings _) = Right $ b
 
-builtin :: String -> [AST] -> AST
-builtin "+" [Number a, Number b] = Number (a + b)
-builtin "-" [Number a, Number b] = Number (a - b)
-builtin "*" [Number a, Number b] = Number (a * b)
-builtin "=" [Binder name, expr] = Bindings (M.singleton name expr)
-builtin "merge" [List binds] =
-  let allBindings = assertBindings <$> binds
-   in Bindings (M.unions allBindings)
+builtin :: String -> [AST] -> Either String AST
+builtin "+" [Number a, Number b] = Right $ Number (a + b)
+builtin "+" args = Left $ "Expected two Number arguments to + but got:" ++ show args
+builtin "-" [Number a, Number b] = Right $ Number (a - b)
+builtin "-" args = Left $ "Expected two Number arguments to - but got:" ++ show args
+builtin "*" [Number a, Number b] = Right $ Number (a * b)
+builtin "*" args = Left $ "Expected two Number arguments to * but got:" ++ show args
+builtin "=" [Binder name, expr] = Right $ Bindings (M.singleton name expr)
+builtin "=" args = Left $ "Expected single expression argument to = but got:" ++ show args
+builtin "merge" [List binds] = do
+  allBindings <- traverse assertBindings binds
+  return . Bindings $ M.unions allBindings
   where
-    assertBindings (Bindings b) = b
-    assertBindings b = error $ "expected bindings; found: " ++ show b
-builtin name args = error $ "no symbol in scope for " ++ name ++ ": " ++ show args
+    assertBindings (Bindings b) = Right b
+    assertBindings b = Left $ "expected bindings; found: " ++ show b
+builtin "merge" args = Left $ "Expected a single list argument to merge but got: " ++ show args
+builtin name args = Left $ "no symbol in scope for " ++ name ++ ": " ++ show args
 
-assertBinders :: AST -> String
-assertBinders (Binder name) = name
-assertBinders b = error $ "expected binding symbol; found: " ++ show b
+assertBinders :: AST -> Either String String
+assertBinders (Binder name) = Right name
+assertBinders b = Left $ "expected binding symbol; found: " ++ show b
--- test/EvalSpec.hs
+++ test/EvalSpec.hs
@@ -12,14 +12,14 @@ spec :: Spec
 spec = do
   describe "Eval" $ do
     it "evals simple arithmetic" $ do
-      eval <$> (parse "(+ 1 2)") `shouldBe` Right (Number 3)
-      eval <$> (parse "(- (+ 1 2) 3)") `shouldBe` Right (Number 0)
+      (parse "(+ 1 2)" >>= eval) `shouldBe` Right (Number 3)
+      (parse "(- (+ 1 2) 3)" >>= eval) `shouldBe` Right (Number 0)
     it "evals functions" $ do
-      eval <$> parse "({[:num] (* num 2)} 5)" `shouldBe` Right (Number 10)
+      (parse "({[:num] (* num 2)} 5)" >>= eval) `shouldBe` Right (Number 10)
     it "evals bindings" $ do
-      ((eval <$> parse "((= :times_two {[:num] (* num 2)}) (times_two 10))") `shouldBe`
+      ((parse "((= :times_two {[:num] (* num 2)}) (times_two 10))" >>= eval) `shouldBe`
         Right (Number 20))
     it "evals recursive functions" $ do
-      eval <$> parse "((= :times_two {[:num] (* num 2)}) (times_two (times_two 10)))" `shouldBe` Right (Number 40)
+      (parse "((= :times_two {[:num] (* num 2)}) (times_two (times_two 10)))" >>= eval) `shouldBe` Right (Number 40)
     it "merges bindings" $ do
-      eval <$> parse "((merge [(= :times_two {[:num] (* num 2)}) (= :inc {[:num] (+ 1 num)})]) (times_two (inc 1)))" `shouldBe` Right (Number 4)
+      (parse "((merge [(= :times_two {[:num] (* num 2)}) (= :inc {[:num] (+ 1 num)})]) (times_two (inc 1)))" >>= eval) `shouldBe` Right (Number 4)
