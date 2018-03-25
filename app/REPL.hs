3d30aba2af64363ea295a094bf7f49f15aa1a373
Chris Penner <christopher.penner@gmail.com>
2018-03-25 00:01:55 -0600

    Switch to haskeline for repl

--- app/REPL.hs
+++ app/REPL.hs
@@ -5,18 +5,27 @@ import RIO
 import Eval
 import Parse
 import TypeInference
-import Data.Text.IO as TIO
 import Data.Text
 import AST
+import System.Console.Haskeline
 
 repl :: IO ()
-repl = do
-  ln <- strip <$> TIO.getLine
-  putStrLn . pack $ 
-    case stripPrefix ":t" ln of
-      Just (strip -> rest) -> 
-        case parse (unpack rest) of
-          Right ast -> either show pretty $ inferType ast
-          Left err -> show err
-      Nothing -> either show pretty (parse (unpack ln) >>= eval)
-  repl
+repl = runInputT defaultSettings loop
+   where
+       loop :: InputT IO ()
+       loop = do
+           minput <- getInputLine "> "
+           case minput of
+               Nothing -> return ()
+               Just "quit" -> return ()
+               Just input -> do outputStrLn $ evalString input
+                                loop
+
+evalString :: String -> String
+evalString ln =
+  case stripPrefix ":t" (pack ln) of
+    Just (strip -> rest) ->
+      case parse (unpack rest) of
+        Right ast -> either show pretty $ inferType ast
+        Left err -> err
+    Nothing -> either show pretty (parse ln >>= eval)
